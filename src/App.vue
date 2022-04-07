7aad80da0d7f6d0dfffafd7074c23de81e7c58c3
rmesh123 <rdanuwar1994@gmail.com>
2022-04-07 10:00:01 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <ul class="nav justify-content-end">
+    <div class="EFS">Eagle Financial Services</div>
+    <li class="nav-item active"><router-link to="/">Home</router-link> |</li>
+    <li class="nav-item">
+      <router-link :to="{ name: 'CustomerList' }">Customers</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link :to="{name: 'InvestmentList'}">Investments</router-link>
+    </li>|
+    <li class="nav-item">
+      <router-link :to="{name: 'StockList'}">Stocks</router-link>
+    </li>|
+    <li class="nav-item" v-if="!authenticated" @click="login">
+      | <router-link :to="{ name: 'Auth' }">Log in</router-link>
+    </li>
+    <li class="nav-item" v-if="!authenticated" @click="register">
+      | <router-link :to="{ name: 'Register' }">Register</router-link>
+    </li>
+    <li
+      class="nav-item .justify-content-end"
+      v-if="authenticated"
+      @click="logout"
+    >
+      | <router-link :to="{ name: 'Auth' }">Logout</router-link>
+    </li>
+  </ul>
+  <router-view />
+</template>
+<script>
+import router from "./router";
+import { APIService } from "./http/APIService";
+const apiService = new APIService();
+
+export default {
+  name: "App",
+  data: () => ({
+    authenticated: false,
+    dialog: false,
+    menu: [
+      { title: "Home", url: "/" },
+      { title: "Customers", url: "/customer-list" },
+      { title: 'Investments', url:"/investment-list" },
+      { title: 'Stocks', url:"/stock-list" },
+    ],
+  }),
+  mounted() {
+    apiService
+      .getCustomerList()
+      .then((response) => {
+        this.authenticated = true;
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          localStorage.removeItem("isAuthenticates");
+          localStorage.removeItem("log_user");
+          localStorage.removeItem("token");
+          this.authenticated = false;
+        }
+      });
+    console.log("this.authenticated--" + this.authenticated);
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("isAuthenticates");
+      localStorage.removeItem("log_user");
+      localStorage.removeItem("token");
+      this.authenticated = false;
+      // router.push('/');
+      window.location = "/";
+    },
+    login() {
+      router.push("/auth");
+    },
+  },
+};
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url("assets/background.jpg");
+  background-size: cover;
+
+}
+#nav {
+  padding: 30px;
+  background-color: white;
+
+  a {
+    font-weight: normal;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: black;
+    }
+  }
+}
+.nav {
+  padding: 1em;
+  background-color: lightblue;
+
+  li {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  a {
+    color: black;
+    padding: 0.5em;
+
+    &.router-link-exact-active {
+      color: cornflowerblue;
+    }
+  }
+
+  .EFS {
+    margin-right: 33em;
+  }
+
+}
+</style>
